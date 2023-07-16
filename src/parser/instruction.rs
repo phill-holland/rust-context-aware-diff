4e068f4dc094a461b38beb90e91c917d1812ce51
Phill <fotofill.holland@googlemail.com>
2023-07-16 19:16:19 +0100

    I have no idea

--- src/parser/instruction.rs
+++ src/parser/instruction.rs
@@ -1,14 +1,45 @@
+use std::collections::VecDeque;
+use std::rc::Rc;
+use std::cell::RefCell;
+
 pub struct Instruction
 {
-    pub value: String
+    pub value: String,
+    pub children: VecDeque<Rc<RefCell<Instruction>>>
 }
+
+//let mut stack:VecDeque<&Instruction> = VecDeque::new();
 
 impl Instruction
 {
     pub fn new() -> Instruction {
         Instruction 
         {
-            value: "".to_string()
+            value: "".to_string(),
+            children: VecDeque::new()
         }
     }
+
+    pub fn push(&mut self, child: Rc<RefCell<Instruction>>) 
+    {
+        self.children.push_back(child);
+    }
+
+    
+    pub fn back(&mut self) -> &Rc<RefCell<Instruction>>
+    {
+        return self.children.back().unwrap();
+    }
+    
+    /*
+    pub fn push(&self, child: Instruction)
+    {
+        if let Some(moo) = &mut self.children
+        {
+            let ww = moo.unwrap();
+            ww.push_back(child);
+        }
+        //self.children.push_back(child);
+    }
+    */
 }
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,4 +1,7 @@
 use crate::parser::instruction::Instruction;
+use std::collections::VecDeque;
+use std::rc::Rc;
+use std::cell::RefCell;
 //mod instruction;
 
 //pub mod parser
@@ -22,10 +25,168 @@ use crate::parser::instruction::Instruction;
     {
         let mut result = Instruction::new();
 
+        let mut stack:VecDeque<&mut Instruction> = VecDeque::new();
+        stack.push_back(&mut result);
+        //std::stack<diff::blocks::instruction*> stack;    
+        //stack.push(&result);
+
+        //char p;
+        let mut p:char = ' ';
+        let mut current:String = String::from("");
+        let mut quote_state = 0;
+        let mut line_state = 0;
+
         for ch in data.chars()
         {
+            if ch == 13 as char { }
+            else if (ch == ' ') && ((p == ' ')||(p == 10 as char)) && (quote_state == 0) { }
+            else if ch == 10 as char
+            {
+                if line_state == 0 { line_state += 1; }
+            }
+            else if ch == '"'
+            {
+                if quote_state == 0 { quote_state += 1; }
+                else if quote_state == 1
+                {
+                    if p != '\\' { quote_state = 0; }
+                }
+                current.push(ch);
+            }        
+            else if ch == '{'
+            {           
+                if line_state == 1 && current.len() > 0
+                {
+                    //let mut child:Instruction = Instruction::new();            
+                    //child.value = current.clone();
+                    //current.clear();
+    
+                    let child = Rc::new(RefCell::new(Instruction::new()));
+                    child.borrow_mut().value = current.clone();
+                    current.clear();
+
+                    stack.back_mut().unwrap().push(child);
+                    //stack.push_back(&mut stack.back().unwrap().back().borrow_mut());//child.borrow_mut());//.borrow().children.back_mut().unwrap());                    
+                    //stack.push_back(stack.back_mut().unwrap().back());
+                    //if let Some(oo) = stack.back_mut() 
+                    //{
+                        //oo.children.push_back(child);
+                    //}
+                    //stack.back_mut().unwrap().children;//.push_back(child);
+                    //stack.top()->children.push_back(child);
+                    //stack.push(&stack.top()->children.back());
+                    
+                    //***stack.push_back(&stack.back());
+    
+                    line_state = 0;
+                }
+            }
+            else if ch == '}'
+            {
+                if current.len() > 0
+                {
+                    let mut child:Instruction = Instruction::new();
+                    child.value = current.clone();
+                    //stack.top()->children.push_back(child);
+                    current.clear();
+                }
+    
+                stack.pop_back();
+            }
+            else if line_state == 1
+            {
+                if current.len() > 0
+                {
+                    let mut child:Instruction = Instruction::new();                    
+                    child.value = current.clone();
+                    //stack.top()->children.push_back(child);
+                    current.clear();
+                }
+    
+                current.push(ch);
+                line_state = 0;
+            }
+            else { current.push(ch); }
+    
+            p = ch.clone();
         }
 
         return result;
+        //return stack.back_mut().unwrap().clone();
     }
 //}
+
+
+/*
+    diff::blocks::instruction result;
+    std::stack<diff::blocks::instruction*> stack;
+    
+    stack.push(&result);
+
+    char p;
+    std::string current;
+    int quote_state = 0, line_state = 0;
+
+    for(std::string::iterator it = data.begin(); it != data.end(); ++it)
+    {        
+        if(*it == 13) { }
+        else if((*it == ' ') && ((p == ' ')||(p == 10)) && (quote_state == 0)) { }
+        else if(*it == 10)
+        {
+            if(line_state == 0) ++line_state;
+        }
+        else if(*it == '"')
+        {
+            if(quote_state == 0) ++quote_state;
+            else if (quote_state == 1)
+            {
+                if(p != '\\') quote_state = 0;
+            }
+            current.push_back(*it);
+        }        
+        else if (*it == '{')
+        {           
+            if((line_state == 1)&&(current.size() > 0))
+            {
+                diff::blocks::instruction child;            
+                child.instr = current;                
+                current.clear();
+
+                stack.top()->children.push_back(child);
+                stack.push(&stack.top()->children.back());
+
+                line_state = 0;
+            }
+        }
+        else if(*it == '}')
+        {
+            if(current.size() > 0)
+            {
+                diff::blocks::instruction child;
+                child.instr = current;
+                stack.top()->children.push_back(child);
+                current.clear();
+            }
+
+            stack.pop();
+        }
+        else if(line_state == 1)
+        {
+            if(current.size() > 0)
+            {
+                diff::blocks::instruction child;
+                child.instr = current;
+                stack.top()->children.push_back(child);
+                current.clear();
+            }
+
+            current.push_back(*it);
+            line_state = 0;
+        }
+        else current.push_back(*it);
+
+        p = *it;
+    }
+    
+    return result;
+    */
